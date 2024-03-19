d12e88afcc60b15636ad373c54c68dc7e4ece606
Taha Ahmed Abdelazim Hussein <71207531+TahaThinks@users.noreply.github.com>
2024-03-19 22:42:10 +0400

    Refinement

--- main.py
+++ main.py
@@ -7,31 +7,32 @@ from tkinter import *
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Password Manager by TahaLearns")
-window.config(padx=20, pady=20)
-password_image = PhotoImage(file="password.png")
+window.config(padx=50, pady=50)
+password_image = PhotoImage(file="logo.png")
 
-canvas = Canvas(width=300, height=240, highlightthickness=0)
-canvas.create_image(150, 150, image=password_image)
-canvas.grid(row=0, column=1)
+canvas = Canvas(width=200, height=200)
+canvas.create_image(100, 100, image=password_image)
+canvas.grid(row=0, column=0, columnspan=3)
 
 # Website Entry [2 Labels]
-website_text = Label(text="Website:")
-website_input = Entry(width=35)
-website_text.grid(row=1, column=0)
-website_input.grid(row=1, column=1, columnspan=2)
+website_label = Label(text="Website:")
+website_entry = Entry(width=42)
+website_label.grid(row=1, column=0)
+website_entry.grid(row=1, column=1, columnspan=2)
 # Email/Username Entry [2 Labels]
-email_text = Label(text="Email/Username:")
-email_input = Entry(width=35)
-email_text.grid(row=2, column=0)
-email_input.grid(row=2, column=1, columnspan=2)
+email_label = Label(text="Email/Username:")
+email_entry = Entry(width=42)
+email_label.grid(row=2, column=0)
+email_entry.grid(row=2, column=1, columnspan=2)
 # Password Entry [3 Labels]
 password_text = Label(text="Password:")
-password_input = Entry(width=21)
+password_entry = Entry(width=23)
 password_generate_button = Button(text="Generate Password")
 password_text.grid(row=3, column=0)
-password_input.grid(row=3, column=1)
+password_entry.grid(row=3, column=1)
 password_generate_button.grid(row=3, column=2)
 # Add Button [1 Label]
-
+add_button = Button(text="Add", width=36)
+add_button.grid(row=4, column=1, columnspan=2)
 
 window.mainloop()
