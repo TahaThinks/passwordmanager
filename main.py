b132e3468d848cb0e56317045bd76e462b5388ee
Taha Ahmed Abdelazim Hussein <71207531+TahaThinks@users.noreply.github.com>
2024-03-19 22:19:09 +0400

    Website Entry

--- main.py
+++ main.py
@@ -10,11 +10,16 @@ window.title("Password Manager by TahaLearns")
 window.config(padx=20, pady=20)
 password_image = PhotoImage(file="password.png")
 
-canvas = Canvas(width=300, height=300, highlightthickness=0)
+canvas = Canvas(width=300, height=240, highlightthickness=0)
 canvas.create_image(150, 150, image=password_image)
-canvas.pack()
+canvas.grid(row=0, column=1)
 
 # Website Entry [2 Labels]
+website_text = Label(text="Website:")
+website_input = Entry(width=35)
+
+website_text.grid(row=1, column=0)
+website_input.grid(row=1, column=1, columnspan=2)
 # Email/Username Entry [2 Labels]
 # Password Entry [3 Labels]
 # Add Button [1 Label]
