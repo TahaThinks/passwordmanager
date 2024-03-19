aab4e8088139d6313e2fb2808a96d49b3f94d98b
Taha Ahmed Abdelazim Hussein <71207531+TahaThinks@users.noreply.github.com>
2024-03-19 21:53:13 +0400

    Config Size

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from tkinter import *
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
+window.title("Password Manager by TahaLearns")
+window.config(height=400, width=400, padx=20, pady=20)
 
 
 window.mainloop()
